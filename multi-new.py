89c3d83a067210452cc9148e56f5bd97de48f939
Bertus van Heerden <bertus.mooikloof@gmail.com>
2026-05-12 07:10:55 +0200

    Simpler model i think

---
+++ multi-new.py
@@ -0,0 +1,127 @@
+import numpy as np
+import h5py
+from matplotlib import pyplot as plt
+from scipy.integrate import solve_ivp
+from scipy.optimize import curve_fit
+
+def kinetic(t, y, k1, k2, k3, k4, k5):
+    K = np.array([[-k5,  k3,    k4],
+                  [0, -k2-k3, k1],
+                  [k5,  k2,   -k1-k4]])
+    return K @ y
+
+
+def modelfunc2(t, k1, k2, k3, k4, k5, y0, t_dark):
+    global sol1, sol2, sol3
+    sol1 = solve_ivp(kinetic, [t[0], t[t_dark]], [0, 0, y0], t_eval=t[0:t_dark], args=[k1, k2, k3, k4, k5])
+    sol2 = solve_ivp(kinetic, [t[t_dark], t[-1]], sol1.y[:, -1], t_eval=t[t_dark:-1], args=[0, k2, k3, 0, k5])
+    return np.concatenate((sol1.y[2], sol2.y[2]))
+
+
+dataset = h5py.File('blinking/10 Feb 2025/Chemical study.h5', 'r')
+
+p0_control = [1 / 2, 1 / 13, 1 / 2, 1 / 100, 1 / 100, 1, 0]
+p0_gco = [1 / 60, 1 / 20, 1 / 20, 1 / 100, 1 / 120, 1, 0]
+
+
+def partint(dataset, partnum=1, p0=p0_control):
+    particle_ = dataset[f'Particle {partnum}']
+    abstimes = particle_['Absolute Times (ns)']
+    # print(particle_.attrs['Description'])
+
+    # print(abstimes[0])
+    # print((abstimes[28018]-abstimes[0])/1e6)
+    # print((abstimes[28019]-abstimes[28018])/1e6)
+    difftime = np.diff(abstimes)
+    boundary_photons = np.where(difftime > 10e6)[0]
+    boundary_times = abstimes[boundary_photons]
+    boundary_times_start = abstimes[boundary_photons + 1]
+    boundary_times_start = np.insert(boundary_times_start, 0, abstimes[0])
+    # print(boundary_times)
+    ms_pulse = (boundary_times - boundary_times_start[:-1]) / 1e6
+    # print(ms_pulse)
+    # print(boundary_times_start)
+
+    pulsephotons = np.diff(boundary_photons)
+    norm_pulsephotons = pulsephotons / ms_pulse[1:]
+    norm_pulsephotons = norm_pulsephotons[6:]
+
+    datapoints = len(norm_pulsephotons) + 1
+    endpoint = datapoints * 0.25
+    t = np.linspace(0, endpoint, datapoints)
+    t_plot = np.linspace(0, endpoint, datapoints * 10)
+
+    norm_pulsephotons = np.interp(t_plot, t[:-1], norm_pulsephotons)[:-1]
+    t_dark = np.argmin(norm_pulsephotons)
+    norm_pulsephotons = norm_pulsephotons / norm_pulsephotons[0]
+
+    def fitfunc(t, k1, k2, k3, k4, k5, y0, t_dark_offset):
+        return modelfunc2(t, k1, k2, k3, k4, 1/120, y0, t_dark+int(t_dark_offset*1e9))
+
+    # popt, pcov = curve_fit(fitfunc, t_plot, norm_pulsephotons,
+    #                        p0=p0,
+    #                        bounds=([0, 0, 0, 0, 0, 0, -40e-9],
+    #                                [np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, 200e-9]))
+
+    popt = [0.5, 1, 0.5, 0.01, 0.01, 1, 0]
+    tau1 = 1 / popt[0]
+    tau2 = 1 / popt[1]
+    tau3 = 1 / popt[2]
+    tau4 = 1 / popt[3]
+    tau5 = 1 / popt[4]
+
+    print(f'Tau1 = {tau1:.1f} s')
+    print(f'Tau2 = {tau2:.1f} s')
+    print(f'Tau3 = {tau3:.1f} s')
+    print(f'Tau5 = {tau5:.1f} s')
+
+    model = fitfunc(t_plot, popt[0], popt[1], popt[2], popt[3], popt[4], popt[5], popt[6])
+
+    return norm_pulsephotons, model, t_plot[:-1]
+
+
+# control, controlfit, t_control = partint(dataset, 1)
+control, controlfit, t_control = partint(dataset, 3, p0=p0_gco)
+# control2 = partint(dataset, 3)
+# control3 = partint(dataset, 4)
+gco, gcofit, t_gco = partint(dataset, 17, p0=p0_gco)
+# gco2 = partint(dataset_gco, 5)
+# gco3 = partint(dataset_gco, 6)
+aa, aafit, t_aa = partint(dataset, 5, p0=p0_gco)
+# aa2 = partint(dataset_aa, 4)
+# aa3 = partint(dataset_aa, 5)
+ph, phfit, t_ph = partint(dataset, 14)
+# ph2 = partint(dataset_ph, 4)
+plt.plot(t_control, control, '-', label='Control')
+# plt.plot(t_control, controlfit, color='C0', label='Control')
+# plt.plot(control2)
+# plt.plot(control3)
+
+# plt.plot(t_c2380, c2380, '-', label='2380 mE')
+# plt.plot(t_c1500, c1500, '-', label='1500 mE')
+# plt.plot(t_c700, c700, '-', label='700 mE')
+
+# plt.plot(t_c2380, c2380fit, color='C1', label='2380 mE')
+# plt.plot(t_c1500, c1500fit, color='C2', label='1500 mE')
+
+# plt.plot(t_c700, c700fit, color='C3', label='700 mE')
+# plt.plot(t_gco, gcofit, color='C1', label='Low Oxygen')
+# plt.plot(gco2)
+# plt.plot(gco3)
+
+plt.plot(t_gco, gco, '-', label='Low Oxygen')
+plt.plot(t_aa, aa, '-', label='Ascorbic Acid')
+plt.plot(t_ph, ph, '-', label='Low pH')
+
+# plt.plot(t_aa, aafit, color='C2', label='Ascorbic Acid')
+# plt.plot(aa2)
+# plt.plot(aa3)
+# plt.plot(t_az, azfit, color='C3', label='Sodium Azide')
+# plt.plot(az2)
+# plt.plot(az3)
+# plt.plot(t_ph, phfit, color='C4', label='Low pH')
+# plt.plot(ph2)
+plt.legend()
+plt.xlabel('Time (s)')
+plt.ylabel('Norm. Fluorescence')
+plt.show()
--- threejumps.py
+++ threejumps.py
@@ -8,9 +8,38 @@ from scipy.optimize import curve_fit
 data_dir = '/home/bertus/Documents/Postdoc/Metings/Suurstofprojek'
 
 timestep = 0.05  # time step of intensity trace in seconds
-p0 = [1 / 20, 1 / 1.55, 1 / 550.7, 1 / 17.5, 0.9, 0.1]
+p0 = [1 / 4, 1 / 6, 1 / 10, 1 / 3, 0.5]
 
-dataset = h5py.File(os.path.join(data_dir, '12 April 2025', '2500uW new.h5'), 'r')
+# dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '5900 uW.h5'), 'r')
+# startind = 4
+# onlen = 19
+
+# dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '3900 uW.h5'), 'r')
+# startind = 1
+# onlen = 29
+
+# dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '2780 uW.h5'), 'r')
+# startind = 3
+# onlen = 45
+
+# dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '1120 uW.h5'), 'r')
+# startind = 1
+# onlen = 97
+
+# dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '520 uW.h5'), 'r')
+# startind = 2
+# onlen = 206
+
+# dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '224 uW.h5'), 'r')
+# startind = 1
+# onlen = 467
+
+dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '90 uW.h5'), 'r')
+startind = 3
+onlen = 690
+
+onlyplot = False
+offlen = 181
 
 
 def kinetic(t, y, k1, k2, k3, k4):
@@ -21,7 +50,7 @@ def kinetic(t, y, k1, k2, k3, k4):
     return K @ y
 
 
-def modelfunc(t, k1, k2, k3, k4, y0, q0, t_dark, t_light, t_dark2, t_light2, t_dark3):
+def modelfunc(t, k1, k2, k3, k4, q0, t_dark, t_light, t_dark2, t_light2, t_dark3):
     sol1 = solve_ivp(kinetic, [t[0], t[t_dark+1]], [0, 0, q0, 1-q0], t_eval=t[0:t_dark+1],
                      args=[k1, k2, k3, k4])
     sol2 = solve_ivp(kinetic, [t[t_dark], t[t_light+1]], sol1.y[:, -1], t_eval=t[t_dark:t_light+1],
@@ -52,7 +81,8 @@ def onetrace(dataset, partnum):
 
     pulsephotons = np.diff(boundary_photons)
     norm_pulsephotons = pulsephotons / ms_pulse[1:]
-    norm_pulsephotons /= np.mean(norm_pulsephotons[6])
+    norm_pulsephotons /= np.mean(norm_pulsephotons[startind])
+    norm_pulsephotons = norm_pulsephotons[np.isfinite(norm_pulsephotons)]
     return norm_pulsephotons[:]
 
 
@@ -65,80 +95,64 @@ def avtrace(dataset, partnums):
 
 def fittrace(dataset, partnums):
 
-    norm_pulsephotons = avtrace(dataset, partnums)[6:]
+    if onlyplot:
+        norm_pulsephotons = avtrace(dataset, partnums)[:]
+    else:
+        norm_pulsephotons = avtrace(dataset, partnums)[startind:]
     datapoints = len(norm_pulsephotons) + 1
     endpoint = datapoints * timestep
     t = np.linspace(0, endpoint, datapoints)
 
-    t_dark = np.argmin(norm_pulsephotons[:len(norm_pulsephotons)//3])  # minimum of first third of trace
-    t_light = t_dark + 300
-    t_dark2 = (np.argmin(norm_pulsephotons[len(norm_pulsephotons) // 3:2 * len(norm_pulsephotons) // 3]) +
-               len(norm_pulsephotons) // 3)  # minimum of second third of trace
-    t_light2 = t_dark2 + 300
-    t_dark3 = np.argmin(norm_pulsephotons[2 * len(norm_pulsephotons) // 3:]) + 2 * len(norm_pulsephotons) // 3  # etc.
-    print(t_light)
+    t_dark = onlen  # np.argmin(norm_pulsephotons[:len(norm_pulsephotons)//3])  # minimum of first third of trace
+    t_light = t_dark + offlen
+    t_dark2 = t_light + onlen  # (np.argmin(norm_pulsephotons[len(norm_pulsephotons) // 3:2 * len(norm_pulsephotons) // 3]) +
+               # len(norm_pulsephotons) // 3)  # minimum of second third of trace
+    t_light2 = t_dark2 + offlen
+    t_dark3 = t_light2 + onlen  # np.argmin(norm_pulsephotons[2 * len(norm_pulsephotons) // 3:]) + 2 * len(norm_pulsephotons) // 3  # etc.
+    print(t_dark, t_light, t_dark2, t_light2, t_dark3)
 
 
-    def fitfunc(t, k1, k2, k3, k4, y0, q0):
-        sol1, sol2, sol3, sol4, sol5, sol6 = modelfunc(t, k1, k2, k3, k4, y0, q0, t_dark,
+    def fitfunc(t, k1, k2, k3, k4, q0):
+        sol1, sol2, sol3, sol4, sol5, sol6 = modelfunc(t, k1, k2, k3, k4, q0, t_dark,
                                                        t_light, t_dark2, t_light2, t_dark3)
         return np.concatenate((sol1.y[2]+sol1.y[3], sol2.y[2][1:]+sol2.y[3][1:], sol3.y[2][1:]+sol3.y[3][1:],
                                sol4.y[2][1:]+sol4.y[3][1:], sol5.y[2][1:]+sol5.y[3][1:], sol6.y[2][1:]+sol6.y[3][1:]))
 
 
-    popt, pcov, *extra = curve_fit(fitfunc, t, norm_pulsephotons, p0=p0, bounds=([0, 0, 0, 0, 0.5, 0],
-                                   [10, 10, 10, 10, 1, 1]), verbose=2)
+    if onlyplot:
+        popt = p0
+    else:
+        popt, pcov, *extra = curve_fit(fitfunc, t, norm_pulsephotons, p0=p0, bounds=([0, 0, 0, 0, 0],
+                                       [10, 10, 10, 10, 1]), verbose=2)
 
     tau1 = 1 / popt[0]
     tau2 = 1 / popt[1]
     tau3 = 1 / popt[2]
     tau4 = 1 / popt[3]
-    y0 = popt[4]
-    q0 = popt[5]
+    q0 = popt[4]
 
     print(f'Tau1 = {tau1:.2f} s')
     print(f'Tau2 = {tau2:.2f} s')
     print(f'Tau3 = {tau3:.2f} s')
     print(f'Tau4 = {tau4:.2f} s')
-    print(f'Y0 = {y0:.2f} cps')
     print(f'Q0 = {q0:.2f} cps')
 
     t_plot = t
-    model = fitfunc(t_plot, popt[0], popt[1], popt[2], popt[3], popt[4], popt[5])
+    if onlyplot:
+        model = None
+    else:
+        model = fitfunc(t_plot, popt[0], popt[1], popt[2], popt[3], popt[4])
 
-    return norm_pulsephotons, model, t_plot[:-1], tau1, tau2, tau3, tau4, y0, q0
+    return norm_pulsephotons, model, t_plot[:-1], tau1, tau2, tau3, tau4, q0
 
 
-norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [1, 3, 5, 6, 7, 8, 9])
-plt.plot(t_225, norm_pulsephotons_225[:], '--', color='gray')
-plt.plot(t_225, model_225[:], '-')
-
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [2])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [3])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [4])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [5])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-# #
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [6])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-#
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [7])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-#
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [8])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-#
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [9])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
-
-# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [10])
-# plt.plot(t_225, norm_pulsephotons_225[:], '--')
+norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [1, 2, 3, 4, 5, 6, 7])#, 8, 9, 10])
+# norm_pulsephotons_225, model_225, t_225, *params_225 = fittrace(dataset, [1, 4, 7, 8, 9, 10])
+if not onlyplot:
+    plt.plot(t_225, norm_pulsephotons_225[:], '--', color='gray')
+    plt.plot(t_225, model_225[:], '-')
+else:
+    plt.plot(norm_pulsephotons_225[:], '--', color='gray')
 
 # plt.grid()
 plt.xlabel('Time (s)')
