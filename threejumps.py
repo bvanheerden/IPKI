18819ec240a4bd1d97f216f01d5a0e3bc0795280
Bertus van Heerden <bertus.mooikloof@gmail.com>
2026-06-11 06:33:24 +0200

    going to try to merge changes from SMS computer

--- threejumps.py
+++ threejumps.py
@@ -38,7 +38,7 @@ dataset = h5py.File(os.path.join(data_dir, 'Maart 2026', 'Power study new', '90 
 startind = 3
 onlen = 690
 
-onlyplot = False
+onlyplot = True
 offlen = 181
 
 
@@ -72,15 +72,18 @@ def onetrace(dataset, partnum):
     abstimes = particle_['Absolute Times (ns)']
     # print(particle_.attrs['Description'])
 
+    print(np.max(abstimes))
     difftime = np.diff(abstimes)
     boundary_photons = np.where(difftime > 20e6)[0]
     boundary_times = abstimes[boundary_photons]
     boundary_times_start = abstimes[boundary_photons + 1]
     boundary_times_start = np.insert(boundary_times_start, 0, abstimes[0])
     ms_pulse = (boundary_times - boundary_times_start[:-1]) / 1e6
+    print(ms_pulse)
 
     pulsephotons = np.diff(boundary_photons)
     norm_pulsephotons = pulsephotons / ms_pulse[1:]
+    print(norm_pulsephotons)
     norm_pulsephotons /= np.mean(norm_pulsephotons[startind])
     norm_pulsephotons = norm_pulsephotons[np.isfinite(norm_pulsephotons)]
     return norm_pulsephotons[:]
